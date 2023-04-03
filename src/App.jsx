8d8e051edd0e561ab860269321a336af4a0761f9
Jortizuwu <joseher2511@gmail.com>
2023-04-03 12:10:40 -0500

    fist commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import CrcComponent from './components/crc'
+function App() {
+  return (
+    <div className='container'>
+      <CrcComponent />
+      <p className='read-the-docs'>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
